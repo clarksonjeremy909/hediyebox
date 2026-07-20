4b844eece261ab38d49c552135e5fce3487609ac
clarksonjeremy909 <jclarkson9@liberty.edu>
2026-07-20 18:44:18 +0300

    Create canli.js

---
+++ canli.js
@@ -0,0 +1,5 @@
+{
+  "supabaseUrl": "https://funiuutuagpwofcqoimb.supabase.co",
+  "anonKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1bml1dXR1YWdwd29mY3FvaW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwMTUwODYsImV4cCI6MjA5ODU5MTA4Nn0.jY6Wu4BOgkhbT8oZqVGkuE7LuLNE8izJKOcIFTmI3ngq",
+  "room": "global"
+}
